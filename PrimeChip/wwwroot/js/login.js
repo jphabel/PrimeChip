e021ca56ea32a6b630f2bfe90fd1e759b1e27af6
Vans Gonzales <vanzmckly@gmail.com>
2026-04-25 12:48:52 +0800

    Finalized login page with password toggle functionality.

---
+++ PrimeChip/wwwroot/js/login.js
@@ -0,0 +1,25 @@
+﻿
+console.log("Login.js loaded");
+function togglePassword() {
+    const toggle = document.getElementById("toggle-password");
+    const passwordInput = document.getElementById("password-input");
+
+    // a function when eye is clicked
+    toggle.addEventListener("click", function () {
+
+        // condition to check the type of password input, changed to text if its true, and if its false, change it back to password
+        if (passwordInput.type === 'password') {
+            passwordInput.type = 'text';
+
+        } else if (passwordInput.type === 'text') {
+            passwordInput.type = 'password';
+        };
+
+        // change between eye and eye-slash icons when clicked
+        toggle.classList.toggle("bi-eye");
+        toggle.classList.toggle("bi-eye-slash");
+
+    });
+};
+
+document.addEventListener("DOMContentLoaded", togglePassword);  
